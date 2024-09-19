bd4c2f80b87a0256463dd21b19c1b83209df162d
0xAstin <astinashler@gmail.com>
2024-09-19 16:04:31 +0700

    Add C# Program Solution

--- Warm-up/Running Sum of 1D Array/c#/Solition.cs
+++ Warm-up/Running Sum of 1D Array/c#/Solition.cs
@@ -1,5 +1,11 @@
 public class Solution {
     public int[] RunningSum(int[] nums) {
         
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] += nums[i - 1];
+        }
+
+        return nums;
+        
     }
 }
