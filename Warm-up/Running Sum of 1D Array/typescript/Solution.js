793e4afd88d2fd9579392dfdc974c1eb351136b2
0xAstin <astinashler@gmail.com>
2024-09-19 16:51:35 +0700

    delete language and focus on some

--- Warm-up/Running Sum of 1D Array/typescript/Solution.js
+++
@@ -1,6 +0,0 @@
-function runningSum(nums) {
-    for (var i = 1; i < nums.length; i++) {
-        nums[i] += nums[i - 1];
-    }
-    return nums;
-}
--- Warm-up/Running Sum of 1D Array/typescript/Solution.ts
+++
@@ -1,7 +0,0 @@
-function calculateRunningSum(nums: number[]): number[] {
-  for (let i = 1; i < nums.length; i++) {
-    nums[i] += nums[i - 1];
-  }
-
-  return nums;
-}
